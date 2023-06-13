fe62b7808be89fc18173a922079fefc88feadd6a
Dino Gazić <dinogazic@Dinos-MacBook-Pro.local>
2023-06-13 19:06:06 +0200

    Reinitializing db

--- TABLES/Events.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Events (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(255) NOT NULL,
-	description VARCHAR(255) NOT NULL,
-	organizerId INT NOT NULL,
-	FOREIGN KEY (organizerId) REFERENCES organizers(id)
-)
--- TABLES/InvitationTokensEvents.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE InvitationTokensEvents (
-  eventId INT NOT NULL,
-  invitationToken VARCHAR(8000) NOT NULL,
-  FOREIGN KEY (eventId) REFERENCES events(id)
-);
